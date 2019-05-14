e85e050a3740f5a1a640071ca55b3a5f0f027f38
tpdtrang <tpdtrang97@gmail.com>
2019-05-14 16:41:39 +0700

    Fix master

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -20,5 +20,15 @@ $(document).ready(function () {
         slidesToShow: 1,
         adaptiveHeight: true
     });
+    wow = new WOW(
+        {
+        boxClass:     'wow',      // default
+        animateClass: 'animated', // default
+        offset:       0,          // default
+        mobile:       true,       // default
+        live:         true        // default
+      }
+      )
+      wow.init();
 
 });
